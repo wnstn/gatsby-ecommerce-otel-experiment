b6e7111b137e349d2587cc242ec0a7e232a7ab95
Winston Hearn <winstonhearn@honeycomb.io>
2023-06-01 17:00:25 -0400

    lots of data shaping

--- src/helpers/telemetry/large-resources.js
+++ src/helpers/telemetry/large-resources.js
@@ -3,11 +3,64 @@ import { trace, context } from '@opentelemetry/api';
 import { hrTime } from '@opentelemetry/core';
 
 export class ResourceTiming extends InstrumentationBase {
-  onReport(entry, parentSpanContext) {
+  
+  _getNavigationTiming() {
+    const navTiming = window.performance.getEntriesByType('navigation');
+    if (navTiming.length === 0) {
+      return setTimeout(this._getNavigationTiming, 1000);
+    }
+
+    navTiming.forEach((timing)=>{
+      console.dir(timing);
+      this.parentSpan.setAttributes({
+        'perf.requestTime': timing.responseStart - timing.requestStart,
+        'perf.loadTime': timing.loadEventEnd - timing.loadEventStart,
+        'perf.domContentLoaded': timing.domContentLoadEventEnd - timing.domContentLoadedEventStart,
+        'perf.transferSize': timing.transferSize,
+        'perf.decodedBodySize': timing.decodedBodySize,
+        'perf.encodedBodySize': timing.encodedBodySize,
+        'perf.domInteractive': timing.domInteractive - timing.fetchStart,
+      })
+    });
+    this.parentSpan.end();
+  }
+
+  _getResourceTiming() {
+    console.log('resource timing');
     const now = hrTime();
+    const resources = window.performance.getEntriesByType('resource');
+    resources.forEach((entry) => {
+      this.onReport(entry, this.context, now);
+    })
+    
+  }
+
+  _onDocumentLoaded() {
+    this.context = trace.setSpan(context.active(), this.parentSpan);
+    
+    const navTiming = this._getNavigationTiming.bind(this);
+    const resourceTiming = this._getResourceTiming.bind(this);
+    requestIdleCallback(resourceTiming, {timeout: 2000});
+    requestIdleCallback(navTiming, {timeout: 2000});
+  }
+
+
+  _waitForPageLoad() {
+    trace.getTracer('app-performance').startActiveSpan('resource-performance', (span)=>{
+      this.parentSpan = span;
+      if (window.document.readyState === 'complete') {
+        this._onDocumentLoaded();
+      } else {
+        this._onDocumentLoaded = this._onDocumentLoaded.bind(this);
+        window.addEventListener('DOMContentLoaded', this._onDocumentLoaded);
+        }
+      })
+    }
+  
+  onReport(entry, parentSpanContext, start) {
     const resourceSpan = trace
       .getTracer('resource-performance')
-      .startSpan(entry.initiatorType, { startTime: now }, parentSpanContext);
+      .startSpan(entry.initiatorType, { startTime:  start + entry.fetchStart}, parentSpanContext);
 
     resourceSpan.setAttributes({
       [`resource.tcp.duration_ms`]: entry.connectEnd - entry.connectStart,
@@ -18,7 +71,7 @@ export class ResourceTiming extends InstrumentationBase {
       [`resource.wire_size`]: entry.transferSize,
       [`resource.name`]: entry.name,
     });
-    resourceSpan.end();
+    resourceSpan.end(start + entry.responseEnd);
   }
 
   enable() {
@@ -26,17 +79,6 @@ export class ResourceTiming extends InstrumentationBase {
       return;
     }
     this.enabled = true;
-
-  
-    const parentSpan = trace.getTracer('resource-performance').startSpan('resource-performance');
-    const ctx = trace.setSpan(context.active(), parentSpan);
-    parentSpan.end();
-
-    setTimeout(()=>{
-      const resources = window.performance.getEntriesByType('resource');
-      resources.forEach((entry) => {
-        this.onReport(entry, ctx);
-      })
-    }, 10000);
-  }
+    this._waitForPageLoad();
+  };
 }
