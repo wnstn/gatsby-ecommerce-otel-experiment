4ef82b985f3cab2e3c6edbad138fa42b99a130e0
Winston Hearn <winstonhearn@honeycomb.io>
2023-06-02 16:39:44 -0400

    reversing logic

--- src/helpers/telemetry/large-resources.js
+++ src/helpers/telemetry/large-resources.js
@@ -85,13 +85,13 @@ export class ResourceTiming extends InstrumentationBase {
 
     trace.getTracer('app-performance').startActiveSpan('resource-performance', (span) => {
       this.parentSpan = span;
-      if (window.document.readyState === 'complete') {
-        console.log('in readystate');
-        this.onDocumentLoaded();
-      } else {
+      if (window.document.readyState === 'loading') {
         console.log('setting event listener');
         const onDocumentLoaded = this.onDocumentLoaded.bind(this);
         window.addEventListener('DOMContentLoaded', onDocumentLoaded);
+      } else {
+        console.log('loaded');
+        this.onDocumentLoaded();
       }
     })
   }
